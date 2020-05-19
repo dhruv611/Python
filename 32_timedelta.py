20b6704b1263ec520b544c58b44cbeeff470cb90
dhruv611 <sharma.dhruv1993@gmail.com>
2020-05-19 11:59:44 +0530

    Working with Time

---
+++ 32_timedelta.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+from datetime import date
+from datetime import time
+from datetime import timedelta
+
+print(timedelta(days = 50, hours = 10, minutes = 15))
+
+obj = datetime.now()
+print("Today:",str(obj))
+
+# create a timedelta that uses more than one argument
+print ("in two weeks and 3 days it will be: " + str(obj + timedelta(weeks=2, days=3)))
+
+# calculate the date 1 week ago, formatted as a string
+t = datetime.now() - timedelta(weeks=1)
+s = t.strftime("%A %B %d, %Y")
+print ("one week ago it was " + s)
+
+### How many days until April Fools' Day?
+
+today = date.today()  # get today's date
+afd = date(today.year, 4, 1)  # get April Fool's for the same year
+# use date comparison to see if April Fool's has already gone for this year
+# if it has, use the replace() function to get the date for next year
+if afd < today:
+  print ("April Fool's day already went by %d days ago" % ((today-afd).days))
+  afd = date(today.year+1,4,1)  # if so, get the date for next year
+
+# Now calculate the amount of time until April Fool's Day
+time_to_afd = afd - today
+print ("It's just", time_to_afd.days, "days until next April Fools' Day!")
